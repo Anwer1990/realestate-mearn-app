a429812292edc922b18a65b7fe2eba352d74836b
Anwer <anwer90web@gmail.com>
2024-06-23 15:18:01 +0100

    add auth controller struct

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const register = (req,res) =>{
+    //db oprations 
+    console.log("register endpoint!");
+}
+
+export const login = (req,res) =>{
+    //db oprations 
+    
+}
+
+export const logout = (req,res) =>{
+    //db oprations 
+    
+}
